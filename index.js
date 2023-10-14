531e9a70a172694519af64e5c4a9b7ca2b01b4e4
HappyJuliana <iuliana2002@mail.ru>
2023-10-14 19:17:29 +0300

    optimization of JavaScript code

--- index.js
+++ index.js
@@ -1,56 +1,105 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const form = document.getElementById('appointment_form');
-
     const inputName = document.getElementById('inputName');
-    const resultName = document.getElementById('resultName');
-
     const inputEmail = document.getElementById('inputEmail');
-    const resultEmail = document.getElementById('resultEmail');
-
     const inputMessage = document.getElementById('inputMessage');
-    const resultMessage = document.getElementById('resultMessage');
-
-    form.addEventListener('submit', handleSubmit)
+    
+    form.addEventListener('submit', handleSubmit);
 
     function handleSubmit(event) {
         event.preventDefault();
 
-        const name = inputName.value;
-        const email = inputEmail.value;
-        const message = inputMessage.value;
+        let formElements = [
+            {
+                element: inputName, 
+                properties: {
+                    value: inputName.value,
+                    successLabel: `Hello, ${inputName.value}!`,
+                    errorLabel: 'Your name should consist of 2 or more characters',
+                    condition: inputName.value.trim().length > 2,
+                },
+            },
+            {
+                element: inputEmail,
+                properties: {
+                    value: inputEmail.value,
+                    successLabel: `Your email ${inputEmail.value} was registered`,
+                    errorLabel: 'Please enter your email',
+                    condition: !!inputEmail.value,
+                },
+            },
+            {
+                element: inputMessage,
+                properties: {
+                    value: inputMessage.value,
+                    successLabel: 'We received your message',
+                    errorLabel: 'Your message should contain at least 3 words',
+                    condition: inputMessage.value.trim().split(' ').length > 2,
+                },
+            },
+        ];
+        
+        const resetClass = (element) => {
+            let classes = [...element.classList];
 
-        if (name.trim() === '') {
-            resultName.textContent = 'Please enter your name';
-            resultName.style.color = 'red';
-        } else if (name.length < 3) {
-            resultName.textContent = 'Your name should consist of 2 or more characters';
-            resultName.style.color = 'red';
-        } else {
-            resultName.textContent = `Hello, ${name}!`
-            resultName.style.color = 'green';
-        };
-
-        if (email === '') {
-            resultEmail.textContent = 'Please enter your email';
-            resultEmail.style.color = 'red';
-        } else {
-            resultEmail.textContent = `Your email ${email} was registered`;
-            resultEmail.style.color = 'green';
-        };
-
-        if (message.trim() === '') {
-            resultMessage.textContent = 'Please enter your message';
-            resultMessage.style.color = 'red';
-        } else {
-            const words = message.split(' ');
-            if (words.length < 3) {
-                resultMessage.textContent = 'Your message should contain at least 3 words';
-                resultMessage.style.color = 'red';
-            } else {
-                resultMessage.textContent = 'We received your message';
-                resultMessage.style.color = 'green';
+            for (let className of classes) {
+                if (className !== 'form__grid-content') {
+                    element.classList.remove(className);
+                }
             }
         };
+
+        const handleSuccess = (element, label = '') => {
+            element.querySelector('.formResult').textContent = label;
+            element.classList.add('success');
+        };
+
+        const handleError = (element, label = '') => {
+            element.querySelector('.formResult').textContent = label;
+            element.classList.add('error');
+        };
+
+        const validateElement = (item) => {
+            const { element, properties } = item;
+            let elementWrapper = element.parentElement;
+            resetClass(elementWrapper);
+            if (properties.condition) {
+                handleSuccess(elementWrapper, properties.successLabel);
+            } else {
+                handleError(elementWrapper, properties.errorLabel);
+            }
+        };
+
+        formElements.forEach((item) => validateElement(item));
+
+/*        const fname = {
+            value: inputName.value,
+            successLabel: `Hello, ${inputName.value}!`,
+            errorLabel: 'Your name should consist of 2 or more characters',
+            condition: inputName.value.trim().length > 2,
+        }
+
+        validateElement(inputName, fname)
+
+        const eml = {
+            value: inputEmail.value,
+            successLabel: `Your email ${inputEmail.value} was registered`,
+            errorLabel: 'Please enter your email',
+            condition: !!inputEmail.value,
+        }
+
+        validateElement(inputEmail, eml);
+
+        const message = {
+            value: inputMessage.value,
+            successLabel: 'We received your message',
+            errorLabel: 'Your message should contain at least 3 words',
+            condition: inputMessage.value.trim().split(' ').length > 2,
+        }
+
+        validateElement(inputMessage, message);*/
+
+       
     }
 });
